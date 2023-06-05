9fe8b7d5d2e1d22d3fe908a4349eca3ebe11ecd5
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-06-06 01:00:08 +0900

    initButton 로직 작성

--- app/src/main/java/com/example/burnyourburnout/presentation/lobby/LobbyFragment.kt
+++ app/src/main/java/com/example/burnyourburnout/presentation/lobby/LobbyFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.example.burnyourburnout.databinding.FragmentLobbyBinding
+import com.example.burnyourburnout.ext.toGone
+import com.example.burnyourburnout.ext.toVisible
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -27,6 +30,31 @@ class LobbyFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initButtons()
+        initViews()
+    }
+
+    private fun initButtons() = with(binding) {
+        emailEditText.addTextChangedListener {
+            val enable = emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()
+            loginButton.isEnabled = enable
+        }
+
+        passwordEditText.addTextChangedListener {
+            val enable = emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()
+            loginButton.isEnabled = enable
+        }
+
+        signinEmailEditText.addTextChangedListener {
+            val enable = signinEmailEditText.text.isNotEmpty() && signinPasswordEditText.text.isNotEmpty()
+            signinButton.isEnabled = enable
+        }
+
+
+        signinPasswordEditText.addTextChangedListener {
+            val enable = signinEmailEditText.text.isNotEmpty() && signinPasswordEditText.text.isNotEmpty()
+            signinButton.isEnabled = enable
+        }
     }
 
     private fun initViews() = with(binding) {
@@ -46,5 +74,23 @@ class LobbyFragment : Fragment() {
                 }
         }
 
+        signInTextView.setOnClickListener {
+            loginViewGroup.toGone()
+            signinViewGroup.toVisible()
+
+            val email = signinEmailEditText.text.toString()
+            val password = signinPasswordEditText.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(requireContext(), "회원가입에 성공했습니다.", Toast.LENGTH_SHORT).show()
+                        loginViewGroup.toVisible()
+                        signinViewGroup.toGone()
+                    } else {
+                        Toast.makeText(requireContext(), "회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
     }
 }
---
+++ app/src/main/java/com/example/burnyourburnout/presentation/lobby/LobbyViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.burnyourburnout.presentation.lobby
+
+import com.example.burnyourburnout.presentation.BaseViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+
+internal class LobbyViewModel: BaseViewModel() {
+
+    val scope: CoroutineScope = MainScope()
+    override fun fetchData(): Job = scope.launch {
+
+    }
+
+
+}
