8b8f053ea15ded5709e73db0a867470752c391fd
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-06-05 22:52:40 +0900

    Firebase Login 로직 작성 중

--- app/src/main/java/com/example/burnyourburnout/presentation/lobby/LobbyFragment.kt
+++ app/src/main/java/com/example/burnyourburnout/presentation/lobby/LobbyFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.burnyourburnout.databinding.FragmentLobbyBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LobbyFragment : Fragment() {
 
@@ -23,6 +27,24 @@ class LobbyFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun initViews() = with(binding) {
+        auth = Firebase.auth
+
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(requireContext(), "환영합니다!", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(requireContext(), "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
 
     }
 }
