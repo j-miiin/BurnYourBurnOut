a426bbd52c19a94e09ec56a9a8c48369fdaecd54
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-06-08 23:49:22 +0900

    MVVM -> MVP 변경 중

--- app/src/main/java/com/example/burnyourburnout/presentation/private_place/PrivatePlaceFragment.kt
+++ app/src/main/java/com/example/burnyourburnout/presentation/private_place/PrivatePlaceFragment.kt
@@ -141,4 +141,20 @@ internal class PrivatePlaceFragment : BaseFragment<CalendarViewModel, FragmentPr
 
         return dayList
     }
+
+    private fun getMockList(): List<DiaryEntity> {
+        val calendar = Calendar.getInstance()
+        calendar.set(2023, 5-1, 1)
+        val lastDay = calendar.getActualMaximum(Calendar.DAY_OF_MONTH)
+
+        var id: Long = 0
+
+        var dayRecordList = ArrayList<DiaryEntity>()
+        for (i in 1..lastDay) {
+            dayRecordList.add(DiaryEntity(id, 2023, 5, i, "", (0..6).random()))
+            id += 1
+        }
+
+        return dayRecordList
+    }
 }
